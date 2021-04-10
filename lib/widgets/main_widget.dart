d4e7d7b875c42f613dc4f2315fb664d56756682c
odrevet <odrevet@gmail.com>
2021-04-10 11:48:42 +0200

    typo

--- lib/widgets/main_widget.dart
+++ lib/widgets/main_widget.dart
@@ -263,7 +263,7 @@ class MainWidgetState extends State<MainWidget> {
                   '''Drevet Olivier built the Kingdomino Score app under the GPL license Version 3. 
 This SERVICE is provided by Drevet Olivier at no cost and is intended for use as is.
 This page is used to inform visitors regarding the policy with the collection, use, and disclosure of Personal Information if anyone decided to use my Service.
-I will not use or share your information with anyone : Kingdomino Score works offline and does not send any informations over a network. ''',
+I will not use or share your information with anyone : Kingdomino Score works offline and does not send any information over a network. ''',
               applicationIcon: Image.asset(
                   'android/app/src/main/res/mipmap-mdpi/ic_launcher.png')))
     ];

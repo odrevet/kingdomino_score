8864c46bd31268dc629cab09c0b1773696d4f17b
odrevet <odrevet@gmail.com>
2021-04-22 23:28:56 +0200

    read the RGB values of the first pixel

--- lib/widgets/cameraPreview.dart
+++ lib/widgets/cameraPreview.dart
@@ -3,7 +3,15 @@ import 'dart:io';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'package:image/image.dart';
+import 'package:image/image.dart' as Image;
+
+
+// image lib uses uses KML color format, convert #AABBGGRR to regular #AARRGGBB
+int abgrToArgb(int argbColor) {
+  int r = (argbColor >> 16) & 0xFF;
+  int b = argbColor & 0xFF;
+  return (argbColor & 0xFF00FF00) | (b << 16) | r;
+}
 
 class TakePictureScreen extends StatefulWidget {
   final CameraDescription? camera;
@@ -53,7 +61,15 @@ class TakePictureScreenState extends State<TakePictureScreen> {
           return GestureDetector(
             onTap: () async {
               final xfile = await _controller!.takePicture();
-              final image = decodeImage(File(xfile.path!).readAsBytesSync());
+              final Image.Image? image = Image.decodeImage(File(xfile.path).readAsBytesSync());
+              double px = 0.0;
+              double py = 0.0;
+              int? pixel32 = image?.getPixelSafe(px.toInt(), py.toInt());  //#AABBGGRR
+              int hex = abgrToArgb(pixel32!);
+              Color color = Color(hex);
+              print(color.red);
+              print(color.green);
+              print(color.blue);
             },
             child: Container(
               width: size,

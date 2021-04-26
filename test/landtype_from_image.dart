9c4a8236a4d22122124d8d52905eeda691ea1691
odrevet <odrevet@gmail.com>
2021-04-26 07:58:52 +0200

    refacore

--- test/landtype_from_image.dart
+++ test/landtype_from_image.dart
@@ -8,36 +8,44 @@ import 'package:kingdomino_score_count/models/picture.dart';
 
 void main() {
   test('Red', () {
-    final Image? redImage = decodeImage(File('test/assets/red.png').readAsBytesSync());
-    List<int> redRGB = averageRGB(redImage!);
+    final Image? image =
+        decodeImage(File('test/assets/red.png').readAsBytesSync());
+    List<int> redRGB = averageRGB(image!, 0, 0, image.width, image.height);
     expect([0xFF, 0x00, 0x00], redRGB);
   });
 
-  test('Blue', (){
-    final Image? blueImage =
-    decodeImage(File('test/assets/blue.png').readAsBytesSync());
-    List<int> blueRGB = averageRGB(blueImage!);
+  test('Blue', () {
+    final Image? image =
+        decodeImage(File('test/assets/blue.png').readAsBytesSync());
+    List<int> blueRGB = averageRGB(image!, 0, 0, image.width, image.height);
     expect([0x00, 0x00, 0xFF], blueRGB);
   });
 
   test('Green', () {
-    final Image? greenImage =
-    decodeImage(File('test/assets/green.png').readAsBytesSync());
-    List<int> greenRGB = averageRGB(greenImage!);
+    final Image? image =
+        decodeImage(File('test/assets/green.png').readAsBytesSync());
+    List<int> greenRGB = averageRGB(image!, 0, 0, image.width, image.height);
     expect([0x00, 0xFF, 0x00], greenRGB);
   });
 
-  test('Multicolor', (){
-    final Image? multiImage =
-    decodeImage(File('test/assets/multi.png').readAsBytesSync());
-    List<int> multiRGB = averageRGB(multiImage!);
+  test('Multicolor', () {
+    final Image? image =
+        decodeImage(File('test/assets/multi.png').readAsBytesSync());
+    List<int> multiRGB = averageRGB(image!, 0, 0, image.width, image.height);
     expect([97, 97, 97], multiRGB);
   });
 
-  test('Wheat', (){
-    final Image? imageWheat =
-    decodeImage(File('test/assets/wheat.jpg').readAsBytesSync());
-    List<int> wheatRGB = averageRGB(imageWheat!);
+  test('Wheat', () {
+    final Image? image =
+        decodeImage(File('test/assets/wheat.jpg').readAsBytesSync());
+    List<int> wheatRGB = averageRGB(image!, 0, 0, image.width, image.height);
     expect(LandType.wheat, getLandtypeFromRGB(wheatRGB));
+  });
+
+  test('Camera', () {
+    final Image? image =
+    decodeImage(File('test/assets/camera.png').readAsBytesSync());
+    List<int> RGB = averageRGB(image!, 0, 0, image.width, image.height);
+    print(RGB);
   });
 }

3fa41ae89974b847b8906efaf65faa54943219e3
odrevet <odrevet@gmail.com>
2022-02-20 20:33:47 +0100

    fix widget_test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:kingdomino_score/main.dart';
+import 'package:kingdomino_score_count/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(KingdominoScore());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);

19432df041f529d99905f15854976a72c7fd0c0d
odrevet <odrevet@gmail.com>
2023-12-03 05:44:23 +0100

    upgrade web build to 3.1.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -2,59 +2,72 @@
 const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
-const RESOURCES = {
-  "index.html": "2147414c01f0b65c0f94cdef048df47d",
-"/": "2147414c01f0b65c0f94cdef048df47d",
-"main.dart.js": "bca4e1b3236f78d35bf29fcd2ba02d9c",
-"assets/android/app/src/main/res/mipmap-mdpi/ic_launcher.png": "988c8873d28b4df062b5dfbc290fa6ff",
+
+const RESOURCES = {"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
+"icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
+"main.dart.js": "ed18f3f33670ab6ab74243b81b8a7286",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"flutter.js": "7d69e653079438abfbb24b82a655b0a4",
+"index.html": "74e969cf4c3901d626945828a59e71f7",
+"/": "74e969cf4c3901d626945828a59e71f7",
+"version.json": "d516deec787071b64c0f1e243a8bdd3e",
+"manifest.json": "2354ea66201148d5e18eff2489654dc6",
 "assets/AssetManifest.json": "77f4b96fa6ff0df1eb91e56cc3587276",
-"assets/assets/lacour/banker.png": "6fd0f06585f9f729ede78b1f1c894f21",
-"assets/assets/lacour/captain.png": "62b8d79f1c8c99aa52b5a499c98244e1",
-"assets/assets/lacour/queen.png": "8c537ad1e71d8adceff3780ea26e0a50",
-"assets/assets/lacour/heavy_archery.png": "7eed659c3d45df1589cda0ba5c0550c8",
-"assets/assets/lacour/axe_warrior.png": "6b2aec5805b8607d5298f847b5e6534b",
-"assets/assets/lacour/fisherman.png": "ffa5cf85384748b752b4ec3797a9a081",
-"assets/assets/lacour/farmer.png": "7fb230794a33706e13c7d81067c1e925",
-"assets/assets/lacour/lumberjack.png": "cb1ffd135afb03957d825d7c9d9d6d67",
-"assets/assets/lacour/shepherdess.png": "54a813508b9131d6319dd08bbb9585e2",
-"assets/assets/lacour/light_archery.png": "4a003a0f8e47700fb22280f7c88f2acf",
-"assets/assets/lacour/sword_warrior.png": "260b1ab9e06b4eb81f6b586e8649e3ff",
-"assets/assets/lacour/resource.png": "1f53c71573bf22419772a4ecc25f916a",
-"assets/assets/lacour/king.png": "dca74aea7980aee489b37d64af41c5cf",
-"assets/assets/king_pawn.png": "6b06194c12d42a5e426dcc6410b3346b",
-"assets/assets/quests/fourCornersMine.svg": "adea4f012d4c5024871206ca3057f091",
-"assets/assets/quests/middleKingdom.svg": "6754c79fb69b696e8a2a99de6b5d20fa",
-"assets/assets/quests/localBusinessGrassLand.svg": "48f40021d31ae2addcf7fc104cb160be",
-"assets/assets/quests/localBusinessMine.svg": "fe89093346336d957e5edb57cf39c844",
-"assets/assets/quests/fourCornersWheat.svg": "ed6e8a460766ea1ef26b3150a00178e2",
-"assets/assets/quests/folieDesGrandeurs.svg": "b8cc6e1d925d641c91860d9fc35e103f",
-"assets/assets/quests/fourCornersGrassLand.svg": "5e260800fc8edb0a66c72edd78081f91",
-"assets/assets/quests/localBusinessForest.svg": "988ac6e998fcf8eaa1aaf0e20e5b25a9",
-"assets/assets/quests/lostCorner.svg": "e4e23983168ef75996c03eaf1312818b",
-"assets/assets/quests/fourCornersLake.svg": "db3fbedb0edbd368079c75be99e2621d",
-"assets/assets/quests/localBusinessLake.svg": "de5cee52cddf1b6e34a0e04938e8a73c",
-"assets/assets/quests/harmony.svg": "771c0c3c0a16a9fb33138121d7acc9b6",
+"assets/NOTICES": "216d19750316a53e4258d1e1e1b407f2",
+"assets/android/app/src/main/res/mipmap-mdpi/ic_launcher.png": "988c8873d28b4df062b5dfbc290fa6ff",
+"assets/AssetManifest.bin.json": "ab953eafdd90dad6f8e9aa0d7b04e303",
+"assets/fonts/HammersmithOne-Regular.ttf": "0e7c18c4a81ec6d5487f4026380a9f62",
+"assets/fonts/MaterialIcons-Regular.otf": "88ea94f08e2a43652aa884a023d602e8",
+"assets/AssetManifest.bin": "4e57817179e10539a22b7e5b45676221",
+"assets/shaders/ink_sparkle.frag": "4096b5150bac93c41cbc9b45276bd90f",
 "assets/assets/quests/fourCornersForest.svg": "a0b0b0a1bd8f4f41b2ca69eed0c0d025",
 "assets/assets/quests/localBusinessSwamp.svg": "5fe3a37c0d9fd122353969bb238882f0",
+"assets/assets/quests/middleKingdom.svg": "6754c79fb69b696e8a2a99de6b5d20fa",
+"assets/assets/quests/fourCornersLake.svg": "db3fbedb0edbd368079c75be99e2621d",
+"assets/assets/quests/localBusinessForest.svg": "988ac6e998fcf8eaa1aaf0e20e5b25a9",
+"assets/assets/quests/lostCorner.svg": "e4e23983168ef75996c03eaf1312818b",
+"assets/assets/quests/harmony.svg": "771c0c3c0a16a9fb33138121d7acc9b6",
+"assets/assets/quests/folieDesGrandeurs.svg": "b8cc6e1d925d641c91860d9fc35e103f",
+"assets/assets/quests/fourCornersSwamp.svg": "7787bcb99605caeb4c491c50a7f43a4f",
+"assets/assets/quests/localBusinessLake.svg": "de5cee52cddf1b6e34a0e04938e8a73c",
+"assets/assets/quests/fourCornersGrassLand.svg": "5e260800fc8edb0a66c72edd78081f91",
+"assets/assets/quests/fourCornersWheat.svg": "ed6e8a460766ea1ef26b3150a00178e2",
+"assets/assets/quests/localBusinessMine.svg": "fe89093346336d957e5edb57cf39c844",
 "assets/assets/quests/localBusinessWheat.svg": "f7aae73a69685e2102df15bdc855563e",
 "assets/assets/quests/bleakKing.svg": "fe88dd2ef329d182381ae3c9f7b433b1",
-"assets/assets/quests/fourCornersSwamp.svg": "7787bcb99605caeb4c491c50a7f43a4f",
+"assets/assets/quests/localBusinessGrassLand.svg": "48f40021d31ae2addcf7fc104cb160be",
+"assets/assets/quests/fourCornersMine.svg": "adea4f012d4c5024871206ca3057f091",
+"assets/assets/king_pawn.png": "6b06194c12d42a5e426dcc6410b3346b",
+"assets/assets/lacour/fisherman.png": "ffa5cf85384748b752b4ec3797a9a081",
+"assets/assets/lacour/resource.png": "1f53c71573bf22419772a4ecc25f916a",
+"assets/assets/lacour/shepherdess.png": "54a813508b9131d6319dd08bbb9585e2",
+"assets/assets/lacour/captain.png": "62b8d79f1c8c99aa52b5a499c98244e1",
+"assets/assets/lacour/light_archery.png": "4a003a0f8e47700fb22280f7c88f2acf",
+"assets/assets/lacour/sword_warrior.png": "260b1ab9e06b4eb81f6b586e8649e3ff",
+"assets/assets/lacour/farmer.png": "7fb230794a33706e13c7d81067c1e925",
+"assets/assets/lacour/king.png": "dca74aea7980aee489b37d64af41c5cf",
+"assets/assets/lacour/heavy_archery.png": "7eed659c3d45df1589cda0ba5c0550c8",
+"assets/assets/lacour/axe_warrior.png": "6b2aec5805b8607d5298f847b5e6534b",
+"assets/assets/lacour/lumberjack.png": "cb1ffd135afb03957d825d7c9d9d6d67",
+"assets/assets/lacour/banker.png": "6fd0f06585f9f729ede78b1f1c894f21",
+"assets/assets/lacour/queen.png": "8c537ad1e71d8adceff3780ea26e0a50",
 "assets/FontManifest.json": "c3f7796b7097ccae3f8ab881706c780e",
-"assets/fonts/HammersmithOne-Regular.ttf": "0e7c18c4a81ec6d5487f4026380a9f62",
-"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"assets/NOTICES": "284de8135352d45cf5272c1af621e0a8",
-"version.json": "1313d3c65e6f6005619c3f3cfe546046"
-};
-
+"canvaskit/skwasm.js": "87063acf45c5e1ab9565dcf06b0c18b8",
+"canvaskit/skwasm.wasm": "4124c42a73efa7eb886d3400a1ed7a06",
+"canvaskit/chromium/canvaskit.js": "0ae8bbcc58155679458a0f7a00f66873",
+"canvaskit/chromium/canvaskit.wasm": "f87e541501c96012c252942b6b75d1ea",
+"canvaskit/canvaskit.js": "eb8797020acdbdf96a12fb0405582c1b",
+"canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
+"canvaskit/canvaskit.wasm": "64edb91684bdb3b879812ba2e48dd487"};
 // The application shell files that are downloaded before a service worker can
 // start.
-const CORE = [
-  "/",
-"main.dart.js",
+const CORE = ["main.dart.js",
 "index.html",
-"assets/NOTICES",
 "assets/AssetManifest.json",
 "assets/FontManifest.json"];
+
 // During install, the TEMP cache is populated with the application shell files.
 self.addEventListener("install", (event) => {
   self.skipWaiting();
@@ -65,7 +78,6 @@ self.addEventListener("install", (event) => {
     })
   );
 });
-
 // During activate, the cache is populated with the temp files downloaded in
 // install. If this service worker is upgrading from one with a saved
 // MANIFEST, then use this to retain unchanged resource files.
@@ -87,6 +99,8 @@ self.addEventListener("activate", function(event) {
         await caches.delete(TEMP);
         // Save the manifest to make future upgrades efficient.
         await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+        // Claim client to enable caching on first launch
+        self.clients.claim();
         return;
       }
       var oldManifest = await manifest.json();
@@ -112,6 +126,8 @@ self.addEventListener("activate", function(event) {
       await caches.delete(TEMP);
       // Save the manifest to make future upgrades efficient.
       await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+      // Claim client to enable caching on first launch
+      self.clients.claim();
       return;
     } catch (err) {
       // On an unhandled exception the state of the cache cannot be guaranteed.
@@ -122,7 +138,6 @@ self.addEventListener("activate", function(event) {
     }
   }());
 });
-
 // The fetch handler redirects requests for RESOURCE files to the service
 // worker cache.
 self.addEventListener("fetch", (event) => {
@@ -151,16 +166,17 @@ self.addEventListener("fetch", (event) => {
     .then((cache) =>  {
       return cache.match(event.request).then((response) => {
         // Either respond with the cached resource, or perform a fetch and
-        // lazily populate the cache.
+        // lazily populate the cache only if the resource was successfully fetched.
         return response || fetch(event.request).then((response) => {
-          cache.put(event.request, response.clone());
+          if (response && Boolean(response.ok)) {
+            cache.put(event.request, response.clone());
+          }
           return response;
         });
       })
     })
   );
 });
-
 self.addEventListener('message', (event) => {
   // SkipWaiting can be used to immediately activate a waiting service worker.
   // This will also require a page refresh triggered by the main worker.
@@ -173,7 +189,6 @@ self.addEventListener('message', (event) => {
     return;
   }
 });
-
 // Download offline will check the RESOURCES for all files not in the cache
 // and populate them.
 async function downloadOffline() {
@@ -194,7 +209,6 @@ async function downloadOffline() {
   }
   return contentCache.addAll(resources);
 }
-
 // Attempt to download the resource online before falling back to
 // the offline cache.
 function onlineFirst(event) {

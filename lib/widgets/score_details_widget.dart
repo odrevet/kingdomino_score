300dbb931a4118fdf73707ca995350a167e34d75
odrevet <odrevet@gmail.com>
2024-02-24 02:11:00 +0100

    remove charcode import

--- lib/widgets/score_details_widget.dart
+++ lib/widgets/score_details_widget.dart
@@ -1,6 +1,5 @@
 import 'dart:collection';
 
-import "package:charcode/charcode.dart";
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:kingdomino_score_count/models/extensions/extension.dart';

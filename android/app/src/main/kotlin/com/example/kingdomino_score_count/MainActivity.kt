5836787ecd0787989807351c43a0310a855c6055
odrevet <odrevet@gmail.com>
2025-07-15 07:13:23 +0200

    upgrade build tools and sdk

---
+++ android/app/src/main/kotlin/com/example/kingdomino_score_count/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.kingdomino_score_count
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/fr/odrevet/kingdomino_score/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package fr.odrevet.kingdomino_score_count
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
